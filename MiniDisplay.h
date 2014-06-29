0444e285c14d47d597f4fde0970206dd9b56c110
Stéphane Lenclud <github@lenclud.com>
2014-06-29 20:38:15 +0200

    Adding CMake file.

---
+++ MiniDisplay.h
@@ -0,0 +1,28 @@
+//
+//
+//
+
+#ifndef MINI_DISPLAY_H
+#define MINI_DISPLAY_H
+
+/**
+*/
+class IMiniDisplay
+	{
+public:
+	
+	};
+
+
+/**
+*/
+class MiniDisplay
+	{
+	static IMiniDisplay* Create();
+
+	};
+
+
+
+#endif
+
