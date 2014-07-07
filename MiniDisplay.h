94592568ed59a38b5b686863d1a2c27209dfa76f
Stéphane Lenclud <github@lenclud.com>
2014-07-07 21:32:47 +0200

    Implementing and C API to control our MiniDisplay.

--- MiniDisplay.h
+++ MiniDisplay.h
@@ -5,23 +5,68 @@
 #ifndef MINI_DISPLAY_H
 #define MINI_DISPLAY_H
 
-/**
+/* Cmake will define MyLibrary_EXPORTS on Windows when it
+configures to build a shared library. If you are going to use
+another build system on windows or create the visual studio
+projects by hand you need to define MyLibrary_EXPORTS when
+building a DLL on windows.
 */
-class IMiniDisplay
-	{
-public:
-	
-	};
+// We are using the Visual Studio Compiler and building Shared libraries
 
+#if defined (_WIN32) 
+  #if defined(MiniDisplay_EXPORTS)
+    #define  MDAPI __declspec(dllexport)
+  #else
+    #define  MDAPI __declspec(dllimport)
+  #endif /* MyLibrary_EXPORTS */
+#else /* defined (_WIN32) */
+ #define MDAPI
+#endif
+
+typedef void* MiniDisplayDevice;
+
+//Open & Close functions
+extern "C" MDAPI MiniDisplayDevice MiniDisplayOpen();
+extern "C" MDAPI void MiniDisplayClose(MiniDisplayDevice aDevice);
 
 /**
+Clear our MiniDisplay.
+@param [IN] The device to apply this command to.
 */
-class MiniDisplay
-	{
-	static IMiniDisplay* Create();
+extern "C" MDAPI void MiniDisplayClear(MiniDisplayDevice aDevice);
 
-	};
+/**
+Fill our MiniDisplay.
+@param [IN] The device to apply this command to.
+*/
+extern "C" MDAPI void MiniDisplayFill(MiniDisplayDevice aDevice);
 
+/**
+Swap our MiniDisplay buffers committing our back buffer content to the screen.
+@param [IN] The device to apply this command to.
+*/
+extern "C" MDAPI void MiniDisplaySwapBuffers(MiniDisplayDevice aDevice);
+
+/**
+Provide maximum brightness level for the given device.
+@param [IN] The device to apply this command to.
+@return Maximum brightness level.
+*/
+extern "C" MDAPI int MiniDisplayMaxBrightness(MiniDisplayDevice aDevice);
+
+/**
+Provide minimum brightness level for the given device.
+@param [IN] The device to apply this command to.
+@return Minimum brightness level.
+*/
+extern "C" MDAPI int MiniDisplayMinBrightness(MiniDisplayDevice aDevice);
+
+/**
+Set device brightness level.
+@param [IN] The device to apply this command to.
+@param [IN] Brightness level
+*/
+extern "C" MDAPI void MiniDisplaySetBrightness(MiniDisplayDevice aDevice, int aBrightness);
 
 
 #endif
